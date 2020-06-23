7ca149ec246ceecfb86296dfd20d3d39c2bec45e
dpillay03 <dpillay03@gmail.com>
2020-06-23 09:31:11 -0700

    config files setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Subreddit Search Engine</h1>
+      </header>
+    </div>
+  );
+}
+
