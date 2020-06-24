c15ddd0550ab509586df53dbeda1847424280209
dpillay03 <dpillay03@gmail.com>
2020-06-23 22:10:58 -0700

    fetch working. items being rendered.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Main from './Main'
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Subreddit Search Engine</h1>
+        <Main />
+
       </header>
     </div>
   );
---
+++ src/components/Main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Search from './Search';
+import Results from './Results';
+
+export default class Main extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            topics: [],
+        }
+        this.performSearch = this.performSearch.bind(this);
+    }
+
+    performSearch(query) {
+        console.log('Query: ', query)
+        fetch(`https://www.reddit.com/r/${query}.json?`)
+        .then(res => {
+            console.log('results : ', res);
+            return res.json();
+        })
+        .then(res => {
+            this.setState({topics: res.data.children })
+        })
+        .catch(err => {
+            console.log('Error: ', err)
+        })
+    }
+    
+    render(){
+        return (
+            <div>
+                <Search search={this.performSearch}/>
+                <Results topics={this.state.topics}/>
+            </div>)
+    }
+}
